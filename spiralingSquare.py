d358c49608336a79d4262d85a3aa706de723245c
Seyithan Çınar <seyithan.cinarr@gmail.com>
2023-10-31 18:44:22 +0300

    spiralSuare revision

--- spiralingSquare.py
+++ spiralingSquare.py
@@ -1,13 +1,25 @@
-import turtle
+import  turtle
 
-drawing_board=turtle.Screen()
-turtle_instance=turtle.Turtle();
-n=5
+turtle_screen=turtle.Screen()
+turtle_instance=turtle.Turtle()
 
-for i in range(n*4):
-    turtle_instance.forward(i*20)
-    turtle_instance.left(90)
+
+def turtle_function (size):
+    for i in range(4):
+        turtle_instance.forward(size)
+        turtle_instance.left(90)
+        size=size-5
+
+
+turtle_function(150)
+turtle_function(130)
+turtle_function(110)
+turtle_function(90)
+turtle_function(70)
+turtle_function(50)
+turtle_function(30)
+turtle_function(10)
+
+
 
 turtle.done()
-
-
